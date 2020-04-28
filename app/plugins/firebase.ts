4b937af5d0f18eb9eed485ceb78a72eafa34008e
roll1226 <tomo_yasu_4724@icloud.com>
2020-04-28 21:13:44 +0900

    first commit

---
+++ app/plugins/firebase.ts
@@ -0,0 +1,34 @@
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/functions'
+import 'firebase/storage'
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+}
+
+export default !firebase.apps.length
+  ? firebase.initializeApp(config)
+  : firebase.app()
+const firestore = firebase.firestore()
+const functions = firebase.functions()
+const storage = firebase.storage()
+const auth = firebase.auth()
+const twitterProvider = new firebase.auth.TwitterAuthProvider()
+const facebookProvider = new firebase.auth.FacebookAuthProvider()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp()
+const fieldValue = firebase.firestore.FieldValue
+
+export { firestore }
+export { auth }
+export { functions }
+export { storage }
+export { twitterProvider }
+export { facebookProvider }
+export { timestamp }
+export { fieldValue }
---
+++ nuxt.config.ts
@@ -0,0 +1,53 @@
+export default {
+  srcDir: 'app/',
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [{ src: '@/plugins/firebase.ts', mode: 'server' }],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ['@nuxt/typescript-build'],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv'
+  ],
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config: any, ctx: any) { // eslint-disable-line
+    }
+  }
+}
