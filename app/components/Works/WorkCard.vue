993bcd3c0c2ace2cfd2eb38855e834e2ea880a66
roll1226 <tomo_yasu_4724@icloud.com>
2020-05-03 01:30:21 +0900

    [UPDATE]flex

---
+++ app/components/Works/WorkCard.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="Card_Wrap">
+    <div class="Card_Img_Wrap">
+      <img :src="work.src" :alt="work.title" />
+    </div>
+
+    <div class="Card_Text_Wrap">
+      <h3 class="Card_Title">
+        {{ work.title }}
+      </h3>
+
+      <p class="Card_Text">
+        {{ work.text }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+type Work = {
+  title: string
+  src: string
+  text: string
+}
+
+export default Vue.extend({
+  props: {
+    work: {
+      type: Object as PropType<Work>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.Card_Wrap {
+  max-width: 100%;
+  margin: 2.5vh auto 2.5vh;
+  background: #fff;
+  border-radius: 10px;
+  box-shadow: 3.5px 3.5px 1.5px rgba(0, 0, 0, 0.1);
+
+  .Card_Img_Wrap {
+    width: 100%;
+    height: 200px;
+    img {
+      width: 100%;
+      max-height: 200px;
+      object-fit: none;
+      object-fit: cover;
+      border-radius: 10px 10px 0 0;
+    }
+  }
+
+  .Card_Text_Wrap {
+    padding: 1.3rem;
+
+    .Card_Title {
+      font-size: 1.7rem;
+      margin-bottom: 1rem;
+    }
+
+    .Card_Text {
+      line-height: 1.5rem;
+      white-space: pre-line;
+    }
+  }
+}
+
+@media screen and (min-width: 770px) {
+  .Card_Wrap {
+    max-width: 25vw;
+    margin: 5vh auto;
+  }
+}
+</style>
--- app/pages/works.vue
+++ app/pages/works.vue
@@ -4,27 +4,8 @@
       作品
     </h2>
 
-    {{ works }}
-
-    <div class="Card_Wrap">
-      <div class="Card_Img_Wrap">
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/j4k1-b789f.appspot.com/o/logo%2FlogoBg.svg?alt=media&token=9f65cd88-4797-4f9a-b567-c2f39907298e"
-          alt=""
-        />
-      </div>
-
-      <div class="Card_Text_Wrap">
-        <h3 class="Card_Title">
-          GLAMPI
-        </h3>
-
-        <p class="Card_Text">
-          2年生のグループ作品。
-          <br />
-          流行していたグランピングの予約サイト。同じ目的を持った人達で集まり、安くとまれるサービスを作った。
-        </p>
-      </div>
+    <div v-for="(work, index) in works" :key="index" class="Works_Card_Wrap">
+      <WorkCard :work="work" />
     </div>
   </div>
 </template>
@@ -32,6 +13,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import { firestore } from '@/plugins/firebase'
+import WorkCard from '~/components/Works/WorkCard.vue'
 
 export interface IWork {
   title: string
@@ -44,6 +26,10 @@ type Data = {
 }
 
 export default Vue.extend({
+  components: {
+    WorkCard
+  },
+
   async asyncData({ store }) {
     await store.commit('nav/SET_IS_NAV', false)
   },
@@ -68,35 +54,10 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
-.Card_Wrap {
-  max-width: 25vw;
-  background: #fff;
-  border-radius: 10px;
-  box-shadow: 3.5px 3.5px 1.5px rgba(0, 0, 0, 0.1);
-
-  .Card_Img_Wrap {
-    width: 100%;
-    height: 200px;
-    img {
-      width: 100%;
-      max-height: 200px;
-      object-fit: none;
-      object-fit: cover;
-      border-radius: 10px 10px 0 0;
-    }
-  }
-
-  .Card_Text_Wrap {
-    padding: 1.3rem;
-
-    .Card_Title {
-      font-size: 1.7rem;
-      margin-bottom: 1rem;
-    }
-
-    .Card_Text {
-      line-height: 1.5rem;
-    }
-  }
+.Works_Card_Wrap {
+  width: 80%;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto;
 }
 </style>
