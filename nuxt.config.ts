82ab50428310f74f6c2f4b294102fa157270a150
roll1226 <tomo_yasu_4724@icloud.com>
2020-05-23 19:11:31 +0900

    [UPDATE]title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: 'プリン好きのポートフォリオ' || '',
+    title: '鶴巻智康のポートフォリオ' || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
