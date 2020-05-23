83a4f09cbb0a4c7e793092e28c69bc4693adc1b2
roll1226 <tomo_yasu_4724@icloud.com>
2020-05-23 20:11:15 +0900

    [UPDATE]head

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="Index_Wrap">
-    <h1 class="Portfolio_Title My_Name">
-      鶴巻智康の
+    <h1 class="Portfolio_Title">
+      鶴巻智康の<br v-if="whatWidth" />ポートフォリオ
     </h1>
-    <h2 class="Portfolio_Title">
-      ポートフォリオ
-    </h2>
 
     <img
       src="~/assets/img/spoon.svg"
@@ -21,6 +18,36 @@ import Vue from 'vue'
 export default Vue.extend({
   async asyncData({ store }) {
     await store.commit('nav/SET_IS_NAV', false)
+  },
+
+  data() {
+    return {
+      whatWidth: false,
+      title: ''
+    }
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('resize', this.calculateWindowWidth)
+    })
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.calculateWindowWidth)
+  },
+  methods: {
+    calculateWindowWidth() {
+      const windowWidth = window.innerWidth
+      if (windowWidth < 770) this.whatWidth = true
+      else this.whatWidth = false
+    }
+  },
+
+  head() {
+    return {
+      title: '鶴巻智康のポートフォリオ',
+      titleTemplate: ''
+    }
   }
 })
 </script>
@@ -43,21 +70,16 @@ export default Vue.extend({
   align-items: center;
 
   .Portfolio_Title {
+    @include fontFamily();
     text-align: center;
     position: relative;
     z-index: 2;
     color: #985633;
     text-shadow: 2.5px 2.5px 7px #b3b3b3;
-    font-size: 2.3rem;
-    letter-spacing: 0.5rem;
     padding-top: 1vh;
     padding-left: 3vw;
-
-    &.My_Name {
-      @include fontFamily();
-      font-size: 2.1rem;
-      letter-spacing: 0.8rem;
-    }
+    font-size: 2.1rem;
+    letter-spacing: 0.8rem;
   }
 
   .Protfolio_Spoon_Img {
@@ -71,15 +93,10 @@ export default Vue.extend({
   .Index_Wrap {
     .Portfolio_Title {
       text-shadow: 4px 4px 5px #b3b3b3;
-      font-size: 4.5rem;
-      letter-spacing: 0.7rem;
+      letter-spacing: 1rem;
+      font-size: 4.1rem;
       padding-top: 0.5vh;
       padding-left: 1vw;
-
-      &.My_Name {
-        letter-spacing: 1rem;
-        font-size: 4.1rem;
-      }
     }
 
     .Protfolio_Spoon_Img {
